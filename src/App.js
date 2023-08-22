7481c9c9be9b13dc1f708d83d0ba990067373413
praveenjd201 <praveenjd201@gmail.com>
2023-08-22 22:40:17 +0530

    initialize page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import DashboardPage from "./pages/Dashboard";
-import { BrowserRouter,Route,Routes } from "react-router-dom";
+import { HashRouter as Router,Route,Routes } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <Router>
         <Routes>
+          <Route path='/' element={<RegisterPage />}/>
           <Route path='/register' element={<RegisterPage />}/>
           <Route path='/dashboard' element={<DashboardPage/>}/>
           <Route path='/login' element={<LoginPage/>} />
-          <Route path='/' element={<h1>fisrt page</h1>}/>  
+         
         </Routes>
 
-      </BrowserRouter>
+      </Router>
       
       
     </div>
